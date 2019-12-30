689d000987958b76dda46ae03adfb19218903f2c
iamtekson <teksondada@gmail.com>
2019-12-30 13:30:34 +0700

    dbconnection to mysql

--- test.php
+++ test.php
@@ -1,3 +1,15 @@
 <?php
-    echo "hello world";
+$servername = "localhost";
+$username = "id11898088_admin";
+$password = "admin";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
+
