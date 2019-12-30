11522e11c6bee53e7cb83ced96e374281ee5ca08
iamtekson <teksondada@gmail.com>
2019-12-30 13:56:24 +0700

    fetch data from database

--- test.php
+++ test.php
@@ -13,3 +13,15 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 ?>
 
+<?php
+    // mysqli_select_db ("id11898088_protofolio");
+mysqli_select_db($conn, 'id11898088_protofolio') or die(mysqli_error($conn));
+
+$sql = "SELECT * FROM myWorks";
+    $result = mysqli_query($conn, $sql);
+
+    while($row = mysqli_fetch_array($result)){
+        echo "id: " . $row["id"]. " - Name: " . $row["title"]. " " . $row["date"]. "<br>";
+    }
+?>
+
