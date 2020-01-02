7bcb74795aaf59bf2522ad7f946cc0f9fa3cf3c3
iamtekson <teksondada@gmail.com>
2020-01-02 14:14:12 +0700

    session added so that unauthorized person can't access the dashboard

--- pages/auth/validation.php
+++ pages/auth/validation.php
@@ -1,16 +1,16 @@
 <?php
     session_start();
     $name = $_POST['uname'];
-    $password = $_POST['psw'];
+    $pass = $_POST['psw'];
     @include('../../_partials/_db_connect.php');
-    $sql = "SELECT * FROM superUser WHERE name = '$name' && password = '$password'";
+    $sql = "SELECT * FROM superUser WHERE name = '$name' && password = '$pass'";
     $result = mysqli_query($conn, $sql);
     $num = mysqli_num_rows($result);
     
     if($num==1) {
-        header('location:../../index.php');
-    } else{
+        $_SESSION['user'] = $name;
         header('location:../dashboard.php');
-    }
-            
+    } else{
+        header('location:../../index.php');
+    }           
 ?>
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if(!isset($_SESSION['user'])) {
+    header('location: ../../index.php')
+}
+?>
 
 <!DOCTYPE html>
 <html>
